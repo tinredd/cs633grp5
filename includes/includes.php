4666210e5e35945c808c6665cb0f9fe0475bed9d
Tina Redd <tina@medhub.com>
2015-11-23 05:37:02 -0500

    renamed the file and updated the main includes file.

--- includes/includes.php
+++ includes/includes.php
@@ -1,6 +1,6 @@
 <?php
 
-include($_SERVER['DOCUMENT_ROOT'].'/includes/connection.php');
+include($_SERVER['DOCUMENT_ROOT'].'/includes/Database.php');
 include($_SERVER['DOCUMENT_ROOT'].'/includes/define.php');
 include($_SERVER['DOCUMENT_ROOT'].'/includes/fx-valid.php');
 include($_SERVER['DOCUMENT_ROOT'].'/includes/variables.php');
