1b3d282825871a224097a3730d352ea4144a11d6
Tina Redd <tina@medhub.com>
2015-11-23 21:59:23 -0500

    Resolve merge conflicts

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-<?php
-
-?>			<footer><?php
+			<footer><?php
 				echo TITLE.'&nbsp;|&nbsp;&copy; '.date('Y');
 			?></footer>
 		</div>
 	</body>
-=======
-<?php
-
-?>
-		</div>
-	</body>
->>>>>>> origin/master
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 
@@ -50,52 +49,4 @@ if ($url!='/index.php' && !isset($_SESSION['employee_id'])) {
 				?>
 			</div>
 		</div>
-=======
-<?php
-session_start();
-if (!isset($includes) || $includes===true) include($_SERVER['DOCUMENT_ROOT'].'/includes/includes.php');
-
-if (array_shift(explode('?',$_SERVER['REQUEST_URI']))!='/index.php' && !isset($_SESSION['employee_id'])) {
-	header('Location: /index.php'); exit();
-}
-?>
-<!doctype html>
-<html>
-	<head>
-		<title><?php echo TITLE; if (strlen(trim($title))>0) echo " :: $title";?></title>
-		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css" />
-		<link rel="stylesheet" href="/css/main.css" />
-		<link rel="stylesheet" href="/css/form.css" />
-		<link rel="stylesheet" href="/css/nav.css" />
-		<link rel="stylesheet" href="/css/table.css" />
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-		<script src="/js/main.js"></script>
-	</head>
-
-	<body>
-		<div class="logo"></div>
-		<div class="topbar">
-		<?php 
-			if ($_SESSION['employee_id']>0) {
-				echo '<div class="logout">
-				Hello <span class="bold">'.stripslashes($_SESSION['first_name']).'</span>! &nbsp;|&nbsp;My Account&nbsp;|&nbsp;
-				<a href="/process/logout.php" onclick="return confirm(\'Are you sure you wish to log out?\');">Logout</a>
-				</div>';
-			}?>
-			<div class="breadcrumbs">
-				<?php 
-			//	Add the "Home" breadcrumb if we are not on the home page...
-				if (array_shift(explode('?',$_SERVER['REQUEST_URI']))!='/index.php') echo '<div style="display:inline-block;"><a href="/index.php">Home</a></div>';
-
-			//	Adding breadcrumbs
-				if (is_array($bcA)) {
-					foreach ($bcA as $url=>$text) echo '<div style="display:inline-block;">&nbsp;&raquo;&nbsp;<a href="'.$url.'">'.stripslashes($text).'</a></div>';
-				}
-				if (strlen(trim($title))>0) echo '<div style="display:inline-block;" class="bold">&nbsp;&raquo;&nbsp;'.stripslashes($title).'</div>';
-				?>
-			</div>
-		</div>
->>>>>>> origin/master
 		<div class="container">
