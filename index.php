60203ca263da6c7a5366a8c696f0530b4f7fabc6
Tina Redd <tina@medhub.com>
2015-11-24 21:42:49 -0500

    Additional styles

--- index.php
+++ index.php
@@ -5,42 +5,42 @@ include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php');
 if ($_SESSION['employee_id']>0) {
 
 ?>
+<div class="standard">Please use the links below to begin your experience.</div>
 <div class="portalpane">
     <div>
         <div class="inline portalheaderimage">i</div>
         <div class="inline">My Information</div>
     </div>
-    <div>
-        <div><a href="/account.php">My account</a></div>
-        <div><a href="/account.php?t=4">My skills</a></div>
-        <div>&nbsp;</div>
-    </div>
+    <ul>
+        <li><a href="/account.php">My account</a></li>
+        <li><a href="/account.php?t=4">My skills</a></li>
+    </ul>
 </div>
 <?php if ($_SESSION['user_type']==1) { ?>
 <div class="portalpane">
     <div>
-        <div class="inline portalheaderimage">&#9873;</div>
+        <div class="inline portalheaderimage">&#9733;</div>
         <div class="inline">Employee Management</div>
     </div>
-    <div>
-        <div><a href="/employee.php">Employees</a></div>
-        <div><a href="/employee.php?action=add">Add employee</a></div>
-    </div>
+    <ul>
+        <li><a href="/employee.php">Employees</a></li>
+        <li><a href="/employee.php?action=add">Add employee</a></li>
+    </ul>
 </div>
 <?php } ?>
 <div class="portalpane">
     <div>
-        <div class="inline portalheaderimage">&#9733;</div>
+        <div class="inline portalheaderimage">&#9873;</div>
         <div class="inline">Jobs Management</div>
     </div>
-    <div>
+    <ul>
         <?php 
         if ($_SESSION['user_type']==1) {
-            echo '<div><a href="/job.php">Jobs</a></div>';
-            echo '<div><a href="/job.php?action=add">Add job</a></div>';
+            echo '<li><a href="/job.php">Jobs</a></li>';
+            echo '<li><a href="/job.php?action=add">Add job</a></li>';
         }
-        ?><div><a href="/jobsearch.php">Search jobs</a></div>
-    </div>
+        ?><li><a href="/jobsearch.php">Search jobs</a></li>
+    </ul>
 </div>
 <?php
 
